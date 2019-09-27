d4ef8249cb29c9d6096076c7eead44203c47dd0a
jakemittlemanwebdev2019 <jmittleman.email@gmail.com>
2019-09-27 09:32:53 -0400

    deployment test

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -12,22 +12,11 @@ class Starter extends React.Component {
     super(props);
     this.state = {
       lastGuess: null,
+      buttonAssignments: // array mix function
+      completed: [],
       matches: 0,
-      grid: [[false,false,false,false],
-            [false,false,false,false],
-            [false,false,false,false],
-            [false,false,false,false]],
     };
   }
-
-  // swap(_ev) {
-  //   let state1 = _.assign({}, this.state, { left: !this.state.left });
-  //   this.setState(state1);
-  // }
-  //
-  // hax(_ev) {
-  //   alert("hax!");
-  // }
 
   resetGame(ev) {
     let state1 = _.assign({}, this.state, { lastGuess: null, matches: 0, })
@@ -63,143 +52,41 @@ class Starter extends React.Component {
     this.setState(state1);
   }
 
-  // buttonPress(ev) {
-  //   // TODO: this should be "if button has been revealed"
-  //   // =========================
-  //   if (ev.target.querySelector('p').style.visibility === "hidden") {
-  //     let targetP = ev.target.querySelector('p');
-  //     let state1 = _.assign({}, this.state, {});
-  //
-  //     // if this is first guess out of two
-  //     if (this.state.lastGuess === null) {
-  //       // visibility style attribution:
-  //       // https://www.w3schools.com/jsref/prop_style_visibility.asp
-  //       targetP.style.visibility = "visible";
-  //       // assign the guess
-  //       state1 = _.assign({}, this.state, { lastGuess: ev.target });
-  //     }
-  //
-  //     // otherwise
-  //     else {
-  //       targetP.style.visibility = "visible";
-  //       // check if the two guesses match
-  //       if (this.state.lastGuess.value === ev.target.value) {
-  //         targetP.style.visibility = "visible";
-  //         state1 = _.assign({}, this.state, {matches: this.state.matches + 1})
-  //       }
-  //       else {
-  //         let oldButton = this.state.lastGuess;
-  //         oldButton.querySelector('p').style.visibility = "hidden";
-  //         targetP.style.visibility = "hidden";
-  //         state1 = _.assign({}, this.state, { lastGuess: null });
-  //       }
-  //     }
-  //     if (this.state.lastGuess != null) {
-  //       console.log("after everything, lastGuess: " + this.state.lastGuess.id)
-  //     } else { console.log("after everything lastGuess is null") }
-  //     this.setState(state1);
-  //   }
-  // }
+  checkEndgame() {
+    let gameState = this.state.grid;
+    let isOver = true;
+    for (var i = 0; i < gameState.length; i++) {
+      for (var j = 0; j < gameState[0].length; j++) {
+        isOver = isOver && gameState[i][j];
+      }
+    }
+    return isOver;
+  }
+
+  buttonClick(i) {
+    let thisButton = document.getElementById(i);
+    let lastButton = document.getElementById(this.state.lastGuess);
+    let state1 = _.assign({}, this.state, {});
+    if (this.state.lastGuess === null) {
+      state1 = _.assign({}, this.state, {lastGuess: i});
+      this.setState(state1);
+    } else {
+      if (thisButton.value === lastButton.value) {
+        let completed = this.state.completed;
+        completed.push(thisButton.value);
+        state1 = _.assign({}, this.state, {completed: completed});
+      }
+      else {
+        state1 = _.assign({}, this.state, {lastGuess: null});
+      }
+      this.setState(state1);
+    }
+  }
+
+
 
   render() {
-    let buttonA1 = <div className="column">
-      <button id="11" value="A" onClick={() => this.buttonPress(this, "11")}>
-        <p style={{visibility: "hidden"}}>A1</p></button>
-      </div>;
-    if (this.state.grid[1][1] === true) {
-      buttonA1 = <div className="column">
-        <button id="11" value="A" onClick={() => this.buttonPress(this, "11")}>
-          <p style={{visibility: "visible"}}>A1</p></button>
-        </div>;
-    }
-    let buttonA2 = <div className="column">
-      <button id="12" value="A" onClick={() => this.buttonPress(this, "12")}>
-        <p style={{visibility: "hidden"}}>A2</p>
-      </button>
-      </div>;
-    if (this.state.grid[1][2] === true) {
-      buttonA2 = <div className="column">
-        <button id="12" value="A" onClick={() => this.buttonPress(this, "12")}>
-          <p style={{visibility: "visible"}}>A2</p>
-        </button>
-        </div>;
-    }
-    let buttonA3 = <div className="column">
-      <button id="13" value = "B" onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A3</p>
-      </button>
-      </div>;
-    let buttonA4 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonB1 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonB2 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonB3 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonB4 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-
-    let buttonC1 = <div className="column">
-      <button value="A" onClick={this.buttonPress.bind(this)}>
-        <p style={{visibility: "hidden"}}>A1</p></button>
-      </div>;
-    let buttonC2 = <div className="column">
-      <button value="A" onClick={this.buttonPress.bind(this)}>
-        <p style={{visibility: "hidden"}}>A2</p>
-      </button>
-      </div>;
-    let buttonC3 = <div className="column">
-      <button value = "B" onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A3</p>
-      </button>
-      </div>;
-    let buttonC4 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonD1 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonD2 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonD3 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-    let buttonD4 = <div className="column">
-      <button onClick={this.buttonPress.bind(this)}>
-      <p style={{visibility: "hidden"}}>A4</p>
-      </button>
-      </div>;
-
-    let resetButton = <div className="column">
-      <button id="reset" onClick={this.resetGame.bind(this)}>
-      <p>Reset Game</p>
-      </button>
-      </div>;
+    let resetButton = <button>DoesNothing</button>;
 
     return (
     <div className="wrapper">
@@ -210,45 +97,65 @@ class Starter extends React.Component {
       </div>
       <div className="row">
         <div className="column">
-          {buttonA1} {buttonB1} {buttonC1} {buttonD1}
+          <BuildButtons root={this} lastGuess={this.state.lastGuess}
+          low={1} high={5} />
         </div>
         <div className="column">
-          {buttonA2} {buttonB2} {buttonC2} {buttonD2}
+          <BuildButtons root={this} lastGuess={this.state.lastGuess}
+          low={5} high={9}/>
         </div>
         <div className="column">
-          {buttonA3} {buttonB3} {buttonC3} {buttonD3}
+          <BuildButtons root={this} lastGuess={this.state.lastGuess}
+          low={9} high={13}/>
         </div>
         <div className="column">
-          {buttonA4} {buttonB4} {buttonC4} {buttonD4}
+          <BuildButtons root={this} lastGuess={this.state.lastGuess}
+          low={13} high={17}/>
         </div>
       </div>
     </div>);
-    // <div className="column">
-    //   <div className="column">{buttonA1} {buttonA2} {buttonA3} {buttonA4}</div>
-    //   <div className="column">{buttonB1} {buttonB2} {buttonB3} {buttonB4}</div>
-    //   <div className="column">{buttonC1} {buttonC2} {buttonC3} {buttonC4}</div>
-    //   <div className="column">{buttonD1} {buttonD2} {buttonD3} {buttonD4}</div>
-    // </div>);
-
-    // let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-    //   <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    // </div>;
-    //
-    // let blank = <div className="column">
-    //   <p>Nothing here.</p>
-    // </div>;
-    //
-    // if (this.state.left) {
-    //   return <div className="column">
-    //     {button}
-    //     {blank}
-    //   </div>;
-    // }
-    // else {
-    //   return <div className="column">
-    //     {blank}
-    //     {button}
-    //   </div>;
-    // }
   }
+}
+
+function returnRandomNums(low, high) {
+  let nums = []
+
+  for (let i = 0; i < 5; i++) {
+    let val = _.random(low, high);
+
+    while (nums.includes(val)) {
+      val = _.random(low, high);
+    }
+    nums.push(val)
+  }
+  return nums
+}
+
+function BuildButtons(props) {
+  let {root} = props;
+  let {lastGuess} = props;
+  let {low} = props;
+  let {high} = props;
+  let nums = _.range(low, high);
+  let completed = root.completed;
+  let letter = "A";
+  // need to not have both buttons turn on at once.
+  let buttons = _.map(nums, (i) => {
+    // if the button's ID has been matched
+    if (root.state.completed.includes(letter)) {
+      return(<button id={i} value={letter} onClick={() => root.buttonClick(i)} key={i}>{letter}</button>);
+    }
+
+    // if the button clicked matches the last button clicked
+    if (i != lastGuess) {
+      return (
+        <button id={i} value={letter} onClick={() => root.buttonClick(i)} key={i}></button>
+      );
+
+    // default case
+    } else {
+      return(<button id={i} value={letter} onClick={() => root.buttonClick(i)} key={i}>{letter}</button>);
+    }
+  });
+  return buttons;
 }
